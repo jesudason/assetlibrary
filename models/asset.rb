9572085db4a25f7de426e1a8b16d7d67a5847cc5
Nicole Jesudason <nicole.jesudason@gmail.com>
2018-04-20 12:21:59 +1000

    resizing graphic again eyeroll

--- models/asset.rb
+++ models/asset.rb
@@ -7,7 +7,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 	include Cloudinary::CarrierWave
 	# process resize_to_fit: [800, 800]
 	version :thumb do
-		process resize_to_fill: [400,400]
+		process resize_to_fill: [350,350]
 	end
 	# storage :file
 
